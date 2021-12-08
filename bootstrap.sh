622a42c2f9ba2f11575b4c44eaf752e16b26b1fe
Roberto Russo <robertorusso.bob@gmail.com>
2021-12-08 16:51:15 +0100

    fix: let bootstrap.sh be more readable

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
 sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io tar bzip2 make automake gcc gcc-c++ pciutils elfutils-libelf-devel libglvnd-devel iptables firewalld vim bind-utils wget git
+sudo yum-config-manager --add-repo \
+https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y \
+docker-ce docker-ce-cli containerd.io tar bzip2 \
+make automake gcc gcc-c++ pciutils \
+elfutils-libelf-devel libglvnd-devel iptables \
+firewalld vim bind-utils wget git
 sudo systemctl start docker
 sudo docker run hello-world
 git clone https://github.com/AlexeyAB/darknet.git
 cd darknet/build/darknet/x64
-curl -L -O https://pjreddie.com/media/files/yolov3.weights
-sudo docker run --rm -v $PWD:/workspace -w /workspace daisukekobayashi/darknet:cpu darknet detector test data/coco.data yolov3.cfg yolov3.weights -i 0 -thresh 0.25 dog.jpg -ext_output
+curl -L -O \
+https://pjreddie.com/media/files/yolov3.weights
+sudo docker run --rm \
+-v $PWD:/workspace -w /workspace \
+daisukekobayashi/darknet:cpu \
+darknet detector test data/coco.data yolov3.cfg \
+yolov3.weights -i 0 -thresh 0.25 \
+dog.jpg -ext_output
 
